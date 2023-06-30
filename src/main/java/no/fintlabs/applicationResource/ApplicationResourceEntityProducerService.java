99a5bdb4ebb53358e29cf4b11c62ad67dfb20e80
Bernt Andreas Johnsen <berntj@viken.no>
2023-06-30 09:24:38 +0200

    add ensureTopic to entityProducer

--- src/main/java/no/fintlabs/applicationResource/ApplicationResourceEntityProducerService.java
+++ src/main/java/no/fintlabs/applicationResource/ApplicationResourceEntityProducerService.java
@@ -4,6 +4,7 @@ import no.fintlabs.kafka.entity.EntityProducer;
 import no.fintlabs.kafka.entity.EntityProducerFactory;
 import no.fintlabs.kafka.entity.EntityProducerRecord;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,15 +12,19 @@ public class ApplicationResourceEntityProducerService {
     private final EntityProducer<ApplicationResourceDTOSimplified> entityProducer;
     private final EntityTopicNameParameters entityTopicNameParameters;
 
-    public ApplicationResourceEntityProducerService(EntityProducerFactory entityProducerFactory) {
+    public ApplicationResourceEntityProducerService(
+            EntityProducerFactory entityProducerFactory,
+            EntityTopicService entityTopicService
+    ) {
         entityProducer = entityProducerFactory.createProducer(ApplicationResourceDTOSimplified.class);
         entityTopicNameParameters = EntityTopicNameParameters
                 .builder()
                 .resource("resource-group")
                 .build();
+        entityTopicService.ensureTopic(entityTopicNameParameters, 0);
     }
     public void publish(ApplicationResource applicationResource) {
-        String key = applicationResource.getResourceId();
+        String key = applicationResource.getId().toString();
         entityProducer.send(
                 EntityProducerRecord.<ApplicationResourceDTOSimplified>builder()
                         .topicNameParameters(entityTopicNameParameters)
