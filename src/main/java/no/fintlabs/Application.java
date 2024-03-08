b444100f3624b92becb105e90043c30420584581
Erling Jahr <erling.jahr@vigoiks.no>
2024-03-08 11:22:33 +0100

    added swagger endpoint

--- src/main/java/no/fintlabs/Application.java
+++ src/main/java/no/fintlabs/Application.java
@@ -1,7 +1,14 @@
 package no.fintlabs;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,6 +17,29 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public OpenAPI openAPIDescription() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FINT Kontroll resource management")
+                        .version("1.0.0")
+                        .description("API's for retrieving resource objects")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .description("Auth key")
+                                .name("Authorization"))
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearer-jwt"))
+                ;
     }
 
 }
