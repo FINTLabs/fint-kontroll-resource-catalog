989f7522a1a62450340dba846e4f3ffddd033b81
Erling Jahr <erling.jahr@vigoiks.no>
2024-12-06 10:13:59 +0100

    updated handhevingstypeLabel enum

--- src/main/java/no/fintlabs/kodeverk/handhevingstype/HandhevingstypeLabels.java
+++ src/main/java/no/fintlabs/kodeverk/handhevingstype/HandhevingstypeLabels.java
@@ -3,7 +3,8 @@ package no.fintlabs.kodeverk.handhevingstype;
 public enum HandhevingstypeLabels {
     HARDSTOP,
     FLOATING,
-    FREE_ALL,
-    FREE_STUDENT,
-    FREE_EDU
+    FREEALL,
+    FREESTUDENT,
+    FREEEDU;
+
 }
