7e9c4a3455d2256934b116b3ca888360c6de9edf
Erling Jahr <erling.jahr@vigoiks.no>
2025-02-20 13:47:40 +0100

    FKS-1138 added resourceAvailability DTOs

---
+++ src/main/java/no/fintlabs/resourceAvailability/ResourceAvailabilityDTO.java
@@ -0,0 +1,14 @@
+package no.fintlabs.resourceAvailability;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResourceAvailabilityDTO {
+    private String resourceId;
+    private Long assignedResources;
+    private ResourceConsumerAssignmentDTO resourceConsumerAssignment;
+}
