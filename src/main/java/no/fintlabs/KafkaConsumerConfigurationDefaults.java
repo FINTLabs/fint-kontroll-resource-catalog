92e15420a2fb6f25d690063bccdf543c0f8ee9ab
Erling Jahr <erling.jahr@novari.no>
2026-05-05 15:32:47 +0200

    FKS-1592 added kafka helper class, Updated applicationResource package consumers to kafka lib ver 6

---
+++ src/main/java/no/fintlabs/KafkaConsumerConfigurationDefaults.java
@@ -0,0 +1,55 @@
+package no.fintlabs;
+
+import no.novari.kafka.consuming.ErrorHandlerConfiguration;
+import no.novari.kafka.consuming.ErrorHandlerFactory;
+import no.novari.kafka.consuming.ListenerConfiguration;
+import no.novari.kafka.topic.name.EntityTopicNameParameters;
+import no.novari.kafka.topic.name.TopicNamePrefixParameters;
+import org.springframework.kafka.listener.CommonErrorHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumerConfigurationDefaults {
+    private final ErrorHandlerFactory errorHandlerFactory;
+
+    public KafkaConsumerConfigurationDefaults(ErrorHandlerFactory errorHandlerFactory) {
+        this.errorHandlerFactory = errorHandlerFactory;
+    }
+
+    public  <T> CommonErrorHandler defaultErrorHandler() {
+
+        return errorHandlerFactory.createErrorHandler(
+                ErrorHandlerConfiguration.<T>stepBuilder()
+                        .noRetries()
+                        .skipFailedRecords()
+                        .build()
+        );
+    }
+
+    public ListenerConfiguration defaultListenerConfiguration() {
+
+        return ListenerConfiguration.stepBuilder()
+                .groupIdApplicationDefault()
+                .maxPollRecordsKafkaDefault()
+                .maxPollIntervalKafkaDefault()
+                .continueFromPreviousOffsetOnAssignment()
+                .build();
+    }
+
+    public TopicNamePrefixParameters defaultTopicNamePrefixParameters() {
+
+        return TopicNamePrefixParameters.stepBuilder()
+                .orgIdApplicationDefault()
+                .domainContextApplicationDefault()
+                .build();
+    }
+
+
+    public EntityTopicNameParameters defaultEntityTopic(String resourceName) {
+
+        return EntityTopicNameParameters.builder()
+                .topicNamePrefixParameters(defaultTopicNamePrefixParameters())
+                .resourceName(resourceName)
+                .build();
+    }
+}
--- src/main/java/no/fintlabs/applicationResource/ApplicationResourceConsumerConfiguration.java
+++ src/main/java/no/fintlabs/applicationResource/ApplicationResourceConsumerConfiguration.java
@@ -1,13 +1,12 @@
 package no.fintlabs.applicationResource;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fintlabs.applicationResourceLocation.ApplicationResourceLocationService;
+import no.fintlabs.KafkaConsumerConfigurationDefaults;
 import no.fintlabs.cache.FintCache;
-import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
-import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
 import no.fintlabs.kodeverk.brukertype.BrukertypeService;
-import no.fintlabs.applicationResourceLocation.ApplicationResourceLocation;
 import no.fintlabs.resourceGroup.AzureGroup;
+import no.novari.kafka.consuming.*;
+import no.novari.kafka.topic.name.EntityTopicNameParameters;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -19,73 +18,91 @@ import java.util.Optional;
 @Slf4j
 @Configuration
 public class ApplicationResourceConsumerConfiguration {
+    private final KafkaConsumerConfigurationDefaults kafkaConsumerConfigurationDefaults;
 
-    private final EntityConsumerFactoryService entityConsumerFactoryService;
-
-    public ApplicationResourceConsumerConfiguration(EntityConsumerFactoryService entityConsumerFactoryService) {
-        this.entityConsumerFactoryService = entityConsumerFactoryService;
+    public ApplicationResourceConsumerConfiguration(KafkaConsumerConfigurationDefaults kafkaConsumerConfigurationDefaults) {
+        this.kafkaConsumerConfigurationDefaults = kafkaConsumerConfigurationDefaults;
     }
+
 
     @Bean
     @ConditionalOnProperty(name = "fint.kontroll.resource-catalog.source", havingValue = "fint")
     public ConcurrentMessageListenerContainer<String,ApplicationResource> applicationResourceConsumer(
             ApplicationResourceService applicationResourceService,
-            EntityConsumerFactoryService entityConsumerFactoryService
+            ErrorHandlerFactory errorHandlerFactory,
+            ParameterizedListenerContainerFactoryService parameterizedListenerContainerFactoryService
     ){
-        EntityTopicNameParameters entityTopicNameParameters = EntityTopicNameParameters
-                .builder()
-                .resource("applicationresource")
-                .build();
-
+        ParameterizedListenerContainerFactory<ApplicationResource> recordListenerContainerFactory =
+                parameterizedListenerContainerFactoryService.createRecordListenerContainerFactory(
+                ApplicationResource.class,
+                (ConsumerRecord<String, ApplicationResource> consumerRecord)
+                        -> applicationResourceService.save(consumerRecord.value()),
+                        kafkaConsumerConfigurationDefaults.defaultListenerConfiguration(),
+                        kafkaConsumerConfigurationDefaults.defaultErrorHandler()
+        );
+        EntityTopicNameParameters entityTopicNameParameters =
+                kafkaConsumerConfigurationDefaults.defaultEntityTopic("applicationresource");
         log.info("Source is FINT. Creating application resource consumer for {}", entityTopicNameParameters);
 
-        return entityConsumerFactoryService.createFactory(
-                ApplicationResource.class,
-                (ConsumerRecord<String,ApplicationResource> consumerRecord)
-                -> applicationResourceService.save(consumerRecord.value()))
-                .createContainer(entityTopicNameParameters);
+        return recordListenerContainerFactory.createContainer(entityTopicNameParameters);
+
     }
+
 
     @Bean
     @ConditionalOnProperty(name = "fint.kontroll.resource-catalog.source", havingValue = "fint")
-    ConcurrentMessageListenerContainer<String, ApplicationResourceUserType> userTypeConsumer(
+    public ConcurrentMessageListenerContainer<String, ApplicationResourceUserType> userTypeConsumer(
             BrukertypeService brukertypeService,
-            EntityConsumerFactoryService entityConsumerFactoryService
-    ){
-        EntityTopicNameParameters entityTopicNameParameters = EntityTopicNameParameters
-                .builder()
-                .resource("applicationresource-usertype")
-                .build();
+            ErrorHandlerFactory errorHandlerFactory,
+            ParameterizedListenerContainerFactoryService parameterizedListenerContainerFactoryService
+    ) {
+        ParameterizedListenerContainerFactory<ApplicationResourceUserType> recordListenerContainerFactory =
+                parameterizedListenerContainerFactoryService.createRecordListenerContainerFactory(
+                        ApplicationResourceUserType.class,
+                        (ConsumerRecord<String, ApplicationResourceUserType> consumerRecord)
+                                -> brukertypeService.save(consumerRecord.value()),
+                        kafkaConsumerConfigurationDefaults.defaultListenerConfiguration(),
+                        kafkaConsumerConfigurationDefaults.defaultErrorHandler()
+                );
 
-        return entityConsumerFactoryService.createFactory(
-                ApplicationResourceUserType.class,
-                (ConsumerRecord<String, ApplicationResourceUserType> consumerRecord)
-                -> brukertypeService.save(consumerRecord.value()))
-                .createContainer(entityTopicNameParameters);
+        EntityTopicNameParameters entityTopicNameParameters =
+                kafkaConsumerConfigurationDefaults.defaultEntityTopic("applicationresource-usertype");
+
+        return recordListenerContainerFactory.createContainer(entityTopicNameParameters);
     }
+
 
     @Bean
     public ConcurrentMessageListenerContainer<String, AzureGroup> azureGroupConsumer(
             FintCache<Long, AzureGroup> azureGroupCache,
-            ApplicationResourceService applicationResourceService
-    ){
-        return entityConsumerFactoryService.createFactory(
-                AzureGroup.class,
-                consumerRecord -> {
-                    AzureGroup azureGroup = consumerRecord.value();
-                    log.debug("Saving: " + azureGroup.getId() + " to cache");
-                    Optional<ApplicationResource> applicationResourceOptional = applicationResourceService.findApplicationResourceById(azureGroup.getResourceGroupID());
+            ApplicationResourceService applicationResourceService,
+            ParameterizedListenerContainerFactoryService parameterizedListenerContainerFactoryService,
+            ErrorHandlerFactory errorHandlerFactory){
+        ParameterizedListenerContainerFactory<AzureGroup> recordListenerContainerFactory =
+                parameterizedListenerContainerFactoryService.createRecordListenerContainerFactory(
+                        AzureGroup.class,
+                        consumerRecord -> {
+                            AzureGroup azureGroup = consumerRecord.value();
+                            log.debug("Saving: " + azureGroup.getId() + " to cache");
+                            Optional<ApplicationResource> applicationResourceOptional =
+                                    applicationResourceService.findApplicationResourceById(azureGroup.getResourceGroupID());
 
-                    if (applicationResourceOptional.isPresent()) {
-                        ApplicationResource applicationResource = applicationResourceOptional.get();
-                        applicationResource.setIdentityProviderGroupObjectId(azureGroup.getId());
-                        applicationResource.setIdentityProviderGroupName(azureGroup.getDisplayName());
-                        log.debug("Saving " + applicationResource.getId() + " with Azure groupObjectId " + azureGroup.getId());
-                        applicationResourceService.save(applicationResource);
-                        azureGroupCache.put(azureGroup.getResourceGroupID(),azureGroup);
-                    }
-                }
-        ).createContainer(EntityTopicNameParameters.builder().resource("azuread-resource-group").build());
-        }
+                            if (applicationResourceOptional.isPresent()) {
+                                ApplicationResource applicationResource = applicationResourceOptional.get();
+                                applicationResource.setIdentityProviderGroupObjectId(azureGroup.getId());
+                                applicationResource.setIdentityProviderGroupName(azureGroup.getDisplayName());
+                                log.debug("Saving " + applicationResource.getId() + " with Azure groupObjectId " + azureGroup.getId());
+                                applicationResourceService.save(applicationResource);
+                                azureGroupCache.put(azureGroup.getResourceGroupID(),azureGroup);
+                            }
+                        },
+                        kafkaConsumerConfigurationDefaults.defaultListenerConfiguration(),
+                        kafkaConsumerConfigurationDefaults.defaultErrorHandler()
+                );
+        EntityTopicNameParameters entityTopicNameParameters =
+                kafkaConsumerConfigurationDefaults.defaultEntityTopic("azuregroup");
+
+        return recordListenerContainerFactory.createContainer(entityTopicNameParameters);
+    }
 }
 
